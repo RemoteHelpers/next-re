7442065b0b07ebfe3dc1a3f8a7f54ec6de1eda9d
Volodymyr Ok <v.shark.t@gmail.com>
2023-04-27 12:49:52 +0300

    added vacancy by slug state

--- src/components/Layout/components/Header/Header.tsx
+++ src/components/Layout/components/Header/Header.tsx
@@ -1,26 +1,32 @@
-import { FC, useEffect, ChangeEvent } from 'react';
+import React, { FC, useEffect, ChangeEvent } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { changeLang } from '@/redux/language/langSlice';
 import { selectLanguage } from '@/redux/language/langSelectors';
-import { getVacancies } from '@/redux/vacancies/vacanciesOperations';
+import { getVacanciesPage, getVacancyBySlug } from '@/redux/vacancies/vacanciesOperations';
 import { AppDispatch } from '@/redux/store';
 import s from './Header.module.scss';
-import { selectVacancies } from '@/redux/vacancies/vacanciesSelectors';
-import axios from 'axios';
+import { selectVacanciesPage, selectVacancyBySlug } from '@/redux/vacancies/vacanciesSelectors';
+// import axios from 'axios';
 
 type Props = {};
 
 export const Header: FC<Props> = ({}) => {
   const dispatch = useDispatch<AppDispatch>();
   const lang = useSelector(selectLanguage);
-  const vacanciesList = useSelector(selectVacancies);
+
+  const vacanciesList = useSelector(selectVacanciesPage);
+  const vacancyBySlug = useSelector(selectVacancyBySlug);
+  console.log('vacancyBySlug >>', vacancyBySlug);
 
   const changeLanguage = (e: ChangeEvent<HTMLSelectElement>): void => {
     dispatch(changeLang(e.target.value));
   };
+  const changeSlug = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    dispatch(getVacancyBySlug({ lang, slug: e.target.value }));
+  };
 
   useEffect(() => {
-    dispatch(getVacancies(lang));
+    dispatch(getVacanciesPage({ lang }));
   }, [lang]);
 
   return (
@@ -31,24 +37,26 @@ export const Header: FC<Props> = ({}) => {
         <option value="EN">US</option>
       </select>
 
+      <input type="text" placeholder="enter slug" onChange={changeSlug} />
+
       {vacanciesList &&
         vacanciesList.map((el: any) => {
-          return <div key={el.id}>{el.attributes.title}</div>;
+          return <div key={el.id}>{el.attributes.vacancySlug}</div>;
         })}
     </header>
   );
 };
 
-export async function getStaticProps() {
-  console.log('entered getStaticProps');
-  const lang = useSelector(selectLanguage);
-  const dispatch = useDispatch<AppDispatch>();
-  await dispatch(getVacancies(lang));
-  const vacanciesList = useSelector(selectVacancies);
-  return {
-    props: {
-      vacanciesList,
-    },
-    revalidate: 10,
-  };
-}
+// export async function getStaticProps() {
+//   console.log('entered getStaticProps');
+//   const lang = useSelector(selectLanguage);
+//   const dispatch = useDispatch<AppDispatch>();
+//   await dispatch(getVacancies(lang));
+//   const vacanciesList = useSelector(selectVacancies);
+//   return {
+//     props: {
+//       vacanciesList,
+//     },
+//     revalidate: 10,
+//   };
+// }
