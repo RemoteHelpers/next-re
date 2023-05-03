2aec4c3f284b0e07ef070c03195285f847f6740f
Ihor Kopieichykov <igor129k02@gmail.com>
2023-05-03 18:04:56 +0300

    basic layout and a little adaptive

--- src/components/Questions/Questions.tsx
+++ src/components/Questions/Questions.tsx
@@ -1,6 +1,5 @@
 
 export const Questions = ({ questions }: any) => {
-  console.log(questions);
   return <h1>Questions - Answers</h1>;
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,28 @@ import { FC } from "react";
 import { Layout } from "@/components/Layout";
 import { Questions } from "@/components/Questions";
 import { getHomeData } from "@/services";
+import { Hero } from "@/components/Hero";
 
 const Home: FC = ({ homeData }: any) => {
-  return (
-    <>
-      <Layout>
-        <h1>Home Page</h1>
-        <Questions questions={homeData} />
-      </Layout>
-    </>
-  );
+	return (
+		<>
+			<Layout>
+        <Hero data={homeData} />
+				<Questions questions={homeData} />
+			</Layout>
+		</>
+	);
 };
 
 export default Home;
 
 export const getServerSideProps = async (context: any) => {
-  const lang = context.locale === "ua" ? "uk" : context.locale;
-  const homeData = await getHomeData(lang);
+	const lang = context.locale === "ua" ? "uk" : context.locale;
+	const homeData = await getHomeData(lang);
 
-  return {
-    props: {
-      homeData,
-    },
-  };
+	return {
+		props: {
+			homeData,
+		},
+	};
 };
