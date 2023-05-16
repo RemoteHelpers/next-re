af78a38177186fc7020f60621a8e1c23422f0d87
Volodymyr Ok <109730758+volodymyr-ok@users.noreply.github.com>
2023-05-16 17:33:14 +0300

    Burger menu (#20)
    * fixed project's building

--- src/pages/[category].tsx
+++ src/pages/[category].tsx
@@ -1,23 +1,32 @@
-import { FC } from "react";
-import { Layout } from "@/components/Layout";
+import { FC } from 'react';
+import { Layout } from '@/components/Layout';
+import { getAllVacancies, getCategories, getFooterData, getHeaderData } from '@/services';
 
-const VacancyPage: FC = ({category}: any) => {
-	return (
-		<Layout>
-			<h1>{category}</h1>
-		</Layout>
-	);
+const VacancyPage: FC<any> = ({ category, categories, vacancies, footerData, navData }) => {
+  return (
+    <Layout footerData={footerData} headerData={{ navData, categories, vacancies }}>
+      <h1>{category}</h1>
+    </Layout>
+  );
 };
 
 export default VacancyPage;
 
 export const getServerSideProps = async (context: any) => {
-	const params = context.params;
-	const categorySlug = params?.category;
-
-	return {
-		props: {
-			category: categorySlug,
-		},
-	};
+  const params = context.params;
+  const categorySlug = params?.category;
+  const lang = context.locale === 'ua' ? 'uk' : context.locale;
+  const categories = await getCategories(lang);
+  const vacancies = await getAllVacancies(lang);
+  const footerData = await getFooterData(lang);
+  const navData = await getHeaderData(lang);
+  return {
+    props: {
+      category: categorySlug,
+      categories,
+      vacancies,
+      footerData,
+      navData,
+    },
+  };
 };
