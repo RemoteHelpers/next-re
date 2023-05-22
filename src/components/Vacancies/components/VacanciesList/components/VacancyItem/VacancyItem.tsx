4196e7bcd594837a03128e8a0d0086501a7938cb
Ihor Kopieichykov <101526474+IhorKopieichykov@users.noreply.github.com>
2023-05-22 16:09:06 +0300

    Category page and a little remarks correction (#29)
    * category page and a little remarks correction

--- src/components/Vacancies/components/VacanciesList/components/VacancyItem/VacancyItem.tsx
+++ src/components/Vacancies/components/VacanciesList/components/VacancyItem/VacancyItem.tsx
@@ -1,48 +1,57 @@
-import React from 'react';
-import s from './VacancyItem.module.scss';
-import { IVacancy, IVacancyAttr } from '@/shared/types';
-import { VacanciesIcon } from '@/shared/components/IconComponents/Vacancies';
-import Link from 'next/link';
+import React, { useCallback } from "react";
+import s from "./VacancyItem.module.scss";
+import { IVacancy, IVacancyAttr } from "@/shared/types";
+import { VacanciesIcon } from "@/shared/components/IconComponents/Vacancies";
+import Link from "next/link";
 
 type Props = {
-  attributes: IVacancyAttr;
-  vacanciesInfo: any;
+	attributes: IVacancyAttr;
+	vacanciesInfo: any;
+	category?: string;
 };
 
-export const VacancyItem: React.FC<Props> = ({ attributes, vacanciesInfo }) => {
-  const {
-    isHot,
-    cardDescription,
-    title,
-    categories: { data: categoriesInfo },
-    vacancySlug,
-  } = attributes;
+export const VacancyItem: React.FC<Props> = ({
+	attributes,
+	vacanciesInfo,
+	category,
+}) => {
+	const { isHot, cardDescription, title, categories, vacancySlug } = attributes;
 
-  return (
-    <li className={s.card}>
-      <div className={s.mainWrap}>
-        <div className={s.titleWrap}>
-          {isHot && (
-            <div className={s.hotLabel}>
-              <VacanciesIcon name="fire" />
-              <p className={s.labelText}>{vacanciesInfo.isHotValue}</p>
-            </div>
-          )}
+	const getPathToVacancy = useCallback((): string => {
+		if (category) {
+			return `/${category}/${vacancySlug}`;
+		}
+		if (categories) {
+			return `/${categories.data[0].attributes.categorySlug}/${vacancySlug}`;
+		}
+		return "";
+	}, [category, vacancySlug]);
 
-          <h3 className={s.title}>{title}</h3>
-        </div>
+	return (
+		<li className={s.card}>
+			<div className={s.mainWrap}>
+				<div className={s.titleWrap}>
+					{isHot && (
+						<div className={s.hotLabel}>
+							<VacanciesIcon name="fire" />
+							<p className={s.labelText}>{vacanciesInfo.isHotValue}</p>
+						</div>
+					)}
 
-        <p className={s.salary}>{vacanciesInfo.salary}</p>
+					<h3 className={s.title}>{title}</h3>
+				</div>
 
-        <p className={s.cardDescription}>{`${cardDescription.slice(0, 107)}...`}</p>
-      </div>
+				<p className={s.salary}>{vacanciesInfo.salary}</p>
 
-      <Link
-        href={`/${categoriesInfo[0].attributes.categorySlug}/${vacancySlug}`}
-        className={s.link}
-      >
-        {vacanciesInfo.button}
-      </Link>
-    </li>
-  );
+				<p className={s.cardDescription}>{`${cardDescription.slice(
+					0,
+					107
+				)}...`}</p>
+			</div>
+
+			<Link href={getPathToVacancy()} className={s.link}>
+				{vacanciesInfo.button}
+			</Link>
+		</li>
+	);
 };
