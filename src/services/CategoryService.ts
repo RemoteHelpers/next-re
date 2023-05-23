b98c7b8642e84fbedccda03f5996e2e2b9a910c0
Ihor Kopieichykov <igor129k02@gmail.com>
2023-05-23 14:20:26 +0300

    basic content layout

--- src/services/CategoryService.ts
+++ src/services/CategoryService.ts
@@ -1,31 +1,33 @@
-import axios from 'axios';
-import { API } from '@/constants';
+import axios from "axios";
+import { API } from "@/constants";
 
 const categoriesInstance = axios.create({
-  baseURL: API,
-  params: {
-    populate: '*',
-  },
+	baseURL: API,
+	params: {
+		populate: "*",
+	},
 });
 
 export const getCategories = async (lang: string) => {
-  try {
-    const res = await categoriesInstance.get(`/categories?locale=${lang}`);
-    return res.data.data as Promise<[]>;
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
+	try {
+		const res = await categoriesInstance.get(`/categories?locale=${lang}`);
+		return res.data.data as Promise<[]>;
+	} catch (error) {
+		console.error(error);
+		return error;
+	}
 };
 
-export const getCategoryBySlug = async (slug: string, lang: string) => {
-  try {
-    const res = await categoriesInstance.get(`/categories?locale=${lang}`);
-    const categories = res.data.data;
-    const category = categories.filter((cat: any) => cat.attributes.categorySlug === slug)[0];
-    return category as Promise<[]>;
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
+export const getCategoryBySlug = async (lang: string, slug: string) => {
+	try {
+		const res = await categoriesInstance.get(`/categories?locale=${lang}`);
+		const categories = res.data.data;
+		const category = categories.filter(
+			(cat: any) => cat.attributes.categorySlug === slug
+		)[0];
+		return category as Promise<[]>;
+	} catch (error) {
+		console.error(error);
+		return error;
+	}
 };
