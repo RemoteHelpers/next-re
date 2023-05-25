a8318c05d0b7045a7e889965b84b1d61a46248a0
Volodymyr Ok <v.shark.t@gmail.com>
2023-05-25 11:30:13 +0300

    cleaned logs

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { API } from '@/constants';
-import type { TFeedbackFormData, TUploadFile } from '@/shared/types/FormFieldsTypes';
 import type { IFeedbackFormData, IUploadFile } from '@/shared/types/FormTypes';
 
 axios.defaults.baseURL = API;
@@ -11,9 +10,7 @@ const Api = {
     id: number;
     attributes: IFeedbackFormData;
   }> {
-    console.log('feedBackForm data', data);
     const res = await axios.post('/form-users', { data });
-
     return res.data.data;
   },
 
@@ -23,13 +20,7 @@ const Api = {
         'Content-Type': 'multipart/form-data',
       },
     });
-    console.log('uploadFile res', res);
-
-    if (res?.data?.length !== 0) {
-      console.log('uploadFile res?.data?.length !== 0 ---> res.data', res.data);
-      return res.data;
-    }
-
+    if (res?.data?.length !== 0) return res.data;
     return [];
   },
 };
