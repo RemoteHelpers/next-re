6764a3a5546996bc410cd036f20d7e748110b8da
Ihor Kopieichykov <igor129k02@gmail.com>
2023-05-25 17:53:18 +0300

    scroll up btn

--- src/components/Layout/components/Main/Main.tsx
+++ src/components/Layout/components/Main/Main.tsx
@@ -1,10 +1,16 @@
 import React, { FC, ReactNode } from "react";
 import s from "./Main.module.scss";
+import { ScrollUpButton } from "@/shared/components/ScrollUpButton";
 
 export type MainProps = {
 	children: ReactNode;
 };
 
 export const Main: FC<MainProps> = ({ children }: MainProps) => {
-	return <main className={s.main}>{children}</main>;
+	return (
+		<main className={s.main}>
+			{children}
+			<ScrollUpButton />
+		</main>
+	);
 };
