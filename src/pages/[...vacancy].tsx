5ae19086eb5718fa7dcfe1073886007640fcf07e
Volodymyr Ok <v.shark.t@gmail.com>
2023-05-31 16:46:09 +0300

    added import for incoming merge

--- src/pages/[...vacancy].tsx
+++ src/pages/[...vacancy].tsx
@@ -1,83 +1,72 @@
-import { FC } from "react";
-import { Layout } from "@/components/Layout";
-import { Vacancy } from "@/components/Vacancy";
-import {
-	getCategoryBySlug,
-	getFormData,
-	getVacancy,
-	getVacancyListData,
-} from "@/services";
-import {
-	getAllVacancies,
-	getCategories,
-	getFooterData,
-	getHeaderData,
-} from "@/services";
-import { VacancyNew } from "@/components/VacancyNew";
+import { FC } from 'react';
+import { Layout } from '@/components/Layout';
+import { Vacancy } from '@/components/Vacancy';
+import { getCategoryBySlug, getFormData, getVacancy, getVacancyListData } from '@/services';
+import { getAllVacancies, getCategories, getFooterData, getHeaderData } from '@/services';
+import { VacancyNew } from '@/components/VacancyNew';
+import { GetServerSidePropsContext } from 'next';
 
 const VacancyPage: FC<any> = ({
-	categories,
-	vacancies,
-	footerData,
-	header,
-	vacancy,
-	vacanciesInfo,
-	category,
-	formData,
+  categories,
+  vacancies,
+  footerData,
+  header,
+  vacancy,
+  vacanciesInfo,
+  category,
+  formData,
 }) => {
-	const { newVersion } = vacancy.attributes;
-	return (
-		<Layout
-			footerData={footerData}
-			headerData={{ header, categories, vacancies }}>
-			{newVersion ? (
-				<VacancyNew
-					vacancy={vacancy}
-					vacanciesInfo={vacanciesInfo}
-					category={category}
-					formData={formData}
-					header={header}
-				/>
-			) : (
-				<Vacancy
-					vacancy={vacancy}
-					vacanciesInfo={vacanciesInfo}
-					category={category}
-					formData={formData}
-					header={header}
-				/>
-			)}
-		</Layout>
-	);
+  const { newVersion } = vacancy.attributes;
+  return (
+    <Layout footerData={footerData} headerData={{ header, categories, vacancies }}>
+      {newVersion ? (
+        <VacancyNew
+          vacancy={vacancy}
+          vacanciesInfo={vacanciesInfo}
+          category={category}
+          formData={formData}
+          header={header}
+        />
+      ) : (
+        <Vacancy
+          vacancy={vacancy}
+          vacanciesInfo={vacanciesInfo}
+          category={category}
+          formData={formData}
+          header={header}
+        />
+      )}
+    </Layout>
+  );
 };
 
 export default VacancyPage;
 
 export const getServerSideProps = async (context: any) => {
-	const params = context.params;
-	// const vacancySlug = params?.vacancy.at(-1);
-	const [categorySlug, vacancySlug] = params?.vacancy;
-	const lang = context.locale;
-	/* queries for layout */
-	const categories = await getCategories(lang);
-	const vacancies = await getAllVacancies(lang);
-	const footerData = await getFooterData(lang);
-	const header = await getHeaderData(lang);
-	/* queries for vacancy */
-	const vacancy = await getVacancy(lang, vacancySlug);
-	const vacanciesInfo = await getVacancyListData(lang);
-	const category = await getCategoryBySlug(lang, categorySlug);
-	const formData = await getFormData(lang);
-	return {
-		props: {
-			categories,
-			vacancies,
-			footerData,
-			header,
-			vacancy,
-			vacanciesInfo,
-			category,
-			formData,
-		},
-	};
+  const params = context.params;
+  // const vacancySlug = params?.vacancy.at(-1);
+  const [categorySlug, vacancySlug] = params?.vacancy;
+  const lang = context.locale;
+  /* queries for layout */
+  const categories = await getCategories(lang);
+  const vacancies = await getAllVacancies(lang);
+  const footerData = await getFooterData(lang);
+  const header = await getHeaderData(lang);
+  /* queries for vacancy */
+  const vacancy = await getVacancy(lang, vacancySlug);
+  const vacanciesInfo = await getVacancyListData(lang);
+  const category = await getCategoryBySlug(lang, categorySlug);
+  const formData = await getFormData(lang);
+  return {
+    props: {
+      categories,
+      vacancies,
+      footerData,
+      header,
+      vacancy,
+      vacanciesInfo,
+      category,
+      formData,
+    },
+  };
 };
