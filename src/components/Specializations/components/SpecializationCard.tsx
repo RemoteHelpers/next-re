e3aaf9b07ec5d3cc887e97e0be4a52e03f3804d0
Volodymyr Ok <v.shark.t@gmail.com>
2023-06-08 17:54:33 +0300

    changed description plug logic

--- src/components/Specializations/components/SpecializationCard.tsx
+++ src/components/Specializations/components/SpecializationCard.tsx
@@ -1,29 +1,28 @@
 import { FC, useContext } from 'react';
 import Link from 'next/link';
 import type { ICategoryAttributes } from '@/shared/types/CategoriesTypes';
+import type { IAbout } from '@/shared/types/AboutTypes';
 import s from './SpecializationCard.module.scss';
 import { SpecializationsIcon } from '@/shared/components/IconComponents/Specializations';
 import { GlobalContext } from '@/context';
 
 type Props = {
   category: ICategoryAttributes;
-  linkText: string;
+  about: IAbout;
 };
 
-export const SpecializationCard: FC<Props> = ({ category, linkText }) => {
+export const SpecializationCard: FC<Props> = ({ category, about }) => {
   const { categorySlug, categoryTitle, shortDescription } = category;
   const { setIsLoading } = useContext(GlobalContext);
   const showLoader = () => setIsLoading(true);
-  const descrPlug =
-    'Мы работаем по шести основным направлениям, которые включают много специальностей. Мы работаем по шести основным направлениям, которые включают много специальностей.';
 
   return (
     <>
       <SpecializationsIcon name={categorySlug} />
       <h3 className={s.title}>{categoryTitle}</h3>
-      <p className={s.description}>{shortDescription || descrPlug}</p>
+      <p className={s.description}>{shortDescription || about.SecondDescription}</p>
       <Link href={`/${categorySlug}`} className={s.link} onClick={showLoader}>
-        <span>{linkText}</span>
+        <span>{about.learnMoreText}</span>
         <SpecializationsIcon name="arrow-more" />
       </Link>
     </>
